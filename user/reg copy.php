2f9876bbb25e7fec6730f2fc435487f1c27bbad4
RyotaMono <ryotamono@gmail.com>
2015-03-10 10:54:53 +0800

    update google ajax

--- user/reg copy.php
+++ user/reg copy.php
@@ -70,7 +70,7 @@ include_once '../lib/config.php';
 
 
 <!-- jQuery 2.0.2 -->
-<script src="http://ajax.useso.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
 <!-- Bootstrap -->
 <script src="../js/bootstrap.min.js" type="text/javascript"></script>
 
